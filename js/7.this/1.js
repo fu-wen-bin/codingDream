cf6a579bc829bdbde153440814faf8bc173abf59
文斌 傅 <fu_wen_bin@outlook.com>
2025-06-19 00:21:42 +0800

    update

---
+++ js/7.this/1.js
@@ -0,0 +1,29 @@
+// 需要给 speak 和 identify 显示传入一个上下文对象
+
+// function identify(context) {
+//   return context.name.toUpperCase()
+// }
+
+// function speak(context) {
+//   var greeting = 'hello, I am ' + identify(context)
+//   console.log(greeting);
+// }
+
+// var me = {
+//   name: 'Tom'
+// }
+// speak(me)
+
+function identify () {
+  return this.name.toUpperCase()
+}
+
+function speak () {
+  var greeting = 'hello, I am ' + identify.call(this)
+  console.log(greeting)
+}
+
+var me = {
+  name: 'Tom',
+}
+speak.call(me)
