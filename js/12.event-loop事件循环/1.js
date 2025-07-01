09c2c3d06dcea9da8db98d01d7b08e4f2803f137
文斌 傅 <fu_wen_bin@outlook.com>
2025-07-02 00:38:40 +0800

    update

---
+++ js/12.event-loop事件循环/1.js
@@ -0,0 +1,13 @@
+// setImmediate(() => {
+//   console.log('setImmediate')
+// })
+
+let a = 1
+
+setTimeout(() => {
+  a = 2
+  console.log(a)
+
+}, 1000)
+
+console.log(a)
---
+++ js/12.event-loop事件循环/async.js
@@ -0,0 +1,23 @@
+function a () {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('a');
+      resolve()
+    }, 1000);
+  })
+}
+
+function b () {
+  console.log('b');
+}
+
+// a().then(() => {
+//   b()
+// })
+
+
+async function foo () {
+  await a()
+  b()
+}
+foo()
