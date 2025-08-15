0b9c03d6678d684f3f5cade8cc60f8854619ffaa
文斌 傅 <fu_wen_bin@outlook.com>
2025-08-15 22:56:05 +0800

    update

---
+++ 面试题/JS系列/10. this/2.js
@@ -0,0 +1,13 @@
+var a = 1
+
+function foo () {
+  var a = 2
+  console.log(this.a)
+}
+
+function bar () {
+  var a = 3
+  foo()
+}
+
+bar()
---
+++ 面试题/JS系列/10. this/3.js
@@ -0,0 +1,11 @@
+var a = 2
+
+function foo () {
+  console.log(this.a)
+}
+
+let obj = {
+  a: 1,
+  foo: foo,
+}
+obj.foo()
