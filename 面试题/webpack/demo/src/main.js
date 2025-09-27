69e33c929ccefb76ed382c2449f03a8ebb2b04c0
文斌 傅 <fu_wen_bin@outlook.com>
2025-09-27 20:39:36 +0800

    update

---
+++ 面试题/webpack/demo/src/main.js
@@ -0,0 +1,14 @@
+import { add } from './add.js';
+console.log(add(1, 2)); // 3
+
+
+// const root = document.getElementById('root');
+// root.innerHTML = 'hello world!!!!!!!!';
+
+// {
+//   variable: 'let',
+//   vname: 'a',
+//   value: 1,
+// }
+
+
